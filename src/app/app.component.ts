5b6ab7717736261c0b74815c12daf18860ac6fb9
Anartz Mugika <mugan86@gmail.com>
2017-03-24 14:42:18 +0100

    Add states with code and name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,34 @@
 import { Component } from '@angular/core';
 import {Http} from '@angular/http';
 
+//To Autocomplete component use
+import {FormControl} from '@angular/forms';
+import 'rxjs/add/operator/startWith';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Angular Material Example';
   spaceScreens: Array<any>;
 
+  stateCtrl: FormControl;
+  filteredStates: any;
+
+  myState = 'AZ';
+ 
+
+  states = [
+    {code: 'AL', name: 'Alabama'},
+    {code: 'AK', name: 'Alaska'},
+    {code: 'AZ', name: 'Arizona'},
+    {code: 'CA', name: 'California'},
+    {code: 'CO', name: 'Colorado'},
+    {code: 'CT', name: 'Connecticut'},
+    {code: 'DE', name: 'Delaware'}
+  ];
 
   constructor(private http:Http) {
     
@@ -17,6 +36,7 @@ export class AppComponent {
       .map(response => response.json().screenshots)
       .subscribe(res => this.spaceScreens = res);
 
+ 
   }
 
   likeMe(i) {
