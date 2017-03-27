37121d7ddea9bcab892f0a11b718d2e1c4f3d491
Anartz Mugika <mugan86@gmail.com>
2017-03-27 08:10:48 +0200

    Start Translate Service and select default language (or select)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {Http} from '@angular/http';
+import {TranslateService} from 'ng2-translate';
 
 //To Autocomplete component use
 import {FormControl} from '@angular/forms';
@@ -37,7 +38,14 @@ export class AppComponent {
     {code: 'DE', name: 'Delaware'}
   ];
 
-  constructor(private http:Http) {
+  constructor(private http:Http, private translate:TranslateService) {
+    console.info("Select language: " + localStorage.getItem('selectLanguage'));
+    if (localStorage.getItem('selectLanguage') == null || localStorage.getItem('selectLanguage') == "null")
+    {
+      localStorage.setItem('selectLanguage', 'en');
+      console.info("Change language to " + localStorage.getItem('selectLanguage'));
+    }
+    translate.setDefaultLang(localStorage.getItem('selectLanguage'));
     
     this.http.get('./src/data.json')
       .map(response => response.json().screenshots)
