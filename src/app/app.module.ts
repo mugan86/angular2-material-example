3053eacb8e31d1011ea500104bf832820d24a088
Anartz Mugika <mugan86@gmail.com>
2017-03-27 08:09:25 +0200

    Add translations imports and add correctly to use

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import {MaterialModule} from '@angular/material';
 
+import { TranslateModule, TranslateLoader  } from 'ng2-translate/ng2-translate';
+import { createTranslateLoader } from './translate-loader';
+import { Http } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import 'hammerjs';
@@ -17,7 +20,12 @@ import 'hammerjs';
     BrowserModule,
     FormsModule,
     HttpModule,
-    MaterialModule.forRoot()
+    MaterialModule.forRoot(),
+    TranslateModule.forRoot({
+      provide: TranslateLoader,
+      useFactory: (createTranslateLoader),
+      deps: [Http]
+    })
   ],
   providers: [],
   bootstrap: [AppComponent],
