d198cc21eee7b04d7592b98312f967f07af6b4bb
Anartz Mugika <mugan86@gmail.com>
2017-04-03 14:13:34 +0200

    Remove unused code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,47 +2,13 @@ import { Component } from '@angular/core';
 import {Http} from '@angular/http';
 import {TranslateService} from 'ng2-translate';
 
-//To Autocomplete component use
-import {FormControl} from '@angular/forms';
-import 'rxjs/add/operator/startWith';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular Material Example';
-
-  //Checkbox
-  checked = false;
-  indeterminate = false;
-  align = 'start';
-  disabled = false;
-  
-  spaceScreens: Array<any>;
-
-  stateCtrl: FormControl;
-  filteredStates: any;
-
-  myState = 'AZ';
  
-
-  states = [
-    {code: 'AL', name: 'Alabama'},
-    {code: 'AK', name: 'Alaska'},
-    {code: 'AZ', name: 'Arizona'},
-    {code: 'CA', name: 'California'},
-    {code: 'CO', name: 'Colorado'},
-    {code: 'CT', name: 'Connecticut'},
-    {code: 'DE', name: 'Delaware'}
-  ];
-
-  languages = [
-    {code: 'en' , label: 'English'},
-    {code: 'es' , label: 'Español'},
-    {code: 'eu' , label: 'Euskara'}
-  ]
 
   language: string = localStorage.getItem('selectLanguage');
 
@@ -55,12 +21,6 @@ export class AppComponent {
     }
 
     translate.setDefaultLang(localStorage.getItem('selectLanguage'));
-
-    this.http.get('./src/data.json')
-      .map(response => response.json().screenshots)
-      .subscribe(res => this.spaceScreens = res);
-
- 
   }
 
 
